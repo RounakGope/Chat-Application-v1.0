7396a21bfca83ae09a7fb690e8deaec84efdd7ab
Rounak Gope <165294367+RounakGope@users.noreply.github.com>
2025-11-08 15:17:13 +0530

    header added

--- Client/Chat-App/src/component/chat.jsx
+++ Client/Chat-App/src/component/chat.jsx
@@ -3,7 +3,22 @@ import toast from 'react-hot-toast'
 
 const chat = () => {
   return (<div>
-    hi
+   
+   <header className='flex flex-row min-w-full justify-between fixed shadow p-4 dark:bg-gray-800 items-center'>
+    <div>
+      <h1>Room No <span>2405751</span></h1>
+
+    </div>
+    <div>
+      <h1>
+        User: <span>Rounak Kumar Gope</span>
+      </h1>
+    </div>
+    <div>
+      <button className=" button py-2 px-3 rounded dark:bg-red-500 dark:hover:bg-red-800 transition-colors duration-300"> Leave Room</button>
+
+    </div>
+   </header>
   </div>
   )
 }
